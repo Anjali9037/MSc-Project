65925cfd6bbfe2e6a691123a20100bd07be0eca8
anjali <anjaligourisankaram@gmail.com>
2024-04-27 09:46:54 +0100

    data

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('landing_page', views.landing_page, name='landing_page'),
+    path('data_addition', views.data_addition, name='data_addition'),
+    path('submit_income', views.submit_income, name='submit_income'),
+    path('submit_expense', views.submit_expense, name='submit_expense'),
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import Post
 
+import pandas as pd
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+
 
 
 
@@ -13,3 +19,58 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
+
+def landing_page(request):
+    return render(request, 'blog/landing_page.html')
+
+
+def data_addition(request):
+    return render(request, 'blog/data_addition.html')
+
+
+
+def submit_income(request):
+    if request.method == 'POST':
+        income_source = request.POST['income_source']
+        income_amount = request.POST['income_amount']
+        income_frequency = request.POST['income_frequency']
+        
+        # Load existing data or create a new DataFrame if the file doesn't exist
+        try:
+            income_df = pd.read_csv('income.csv')
+        except FileNotFoundError:
+            income_df = pd.DataFrame(columns=['Source', 'Amount', 'Frequency'])
+        
+        # Append new data to the DataFrame
+        new_row = {'Source': income_source, 'Amount': income_amount, 'Frequency': income_frequency}
+        income_df = income_df.append(new_row, ignore_index=True)
+        
+        # Save the DataFrame back to CSV
+        income_df.to_csv('income.csv', index=False)
+        
+        return HttpResponse("Income submitted successfully!")
+    else:
+        return HttpResponse("Invalid request method!")
+
+def submit_expense(request):
+    if request.method == 'POST':
+        expense_category = request.POST['expense_category']
+        expense_amount = request.POST['expense_amount']
+        expense_date = request.POST['expense_date']
+        
+        # Load existing data or create a new DataFrame if the file doesn't exist
+        try:
+            expense_df = pd.read_csv('expense.csv')
+        except FileNotFoundError:
+            expense_df = pd.DataFrame(columns=['Category', 'Amount', 'Date'])
+        
+        # Append new data to the DataFrame
+        new_row = {'Category': expense_category, 'Amount': expense_amount, 'Date': expense_date}
+        expense_df = expense_df.append(new_row, ignore_index=True)
+        
+        # Save the DataFrame back to CSV
+        expense_df.to_csv('expense.csv', index=False)
+        
+        return HttpResponse("Expense submitted successfully!")
+    else:
+        return HttpResponse("Invalid request method!")
